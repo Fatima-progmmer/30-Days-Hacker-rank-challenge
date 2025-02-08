8cc269db336e21a79112bc240cc19220ae29217e
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2025-02-08 13:22:03 +0500

    Create Day 1.cpp

---
+++ Day 1/Day 1.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#include <string>
+int main() {
+    string input_string; 
+    getline(cin, input_string); // Read input from stdin
+
+    cout << "Hello, World." << endl; // Print "Hello, World."
+    cout << input_string << endl; // Print the input string
+
+    return 0;
+}
